ce61d197c70acdf2c75c36ac228b2e5199e9d3d2
Aisswaryya Murugananth <39556471+aisswaryya@users.noreply.github.com>
2021-02-18 15:14:09 -0800

    Update 1732.java

--- 1732.java
+++ 1732.java
@@ -2,7 +2,7 @@ class Solution {
     public int largestAltitude(int[] gain) {
         int n= gain.length;
         int temp =0;
-        int max = Integer.MIN_VALUE;
+        int max = 0;
         for(int i=0;i<n;i++){
            temp +=gain[i];
             max = Math.max(temp,max); 
